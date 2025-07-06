be909057058b2205fbe7cfaee349f55c271798c0
sijunkim <papaya9349@naver.com>
2025-07-06 19:24:51 +0900

    LogScope project initialize

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "logscope-backend"
---
+++ backend/src/test/kotlin/com/logscope/logscope/backend/LogscopeBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.logscope.logscope.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogscopeBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
